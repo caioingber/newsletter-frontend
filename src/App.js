136b0b43b65c57b9290fda3481f4c0fc7f9b4fe5
caio_ingber <caioingber@gmail.com>
2020-03-27 09:58:21 -0400

    installed dependencies

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div className="App">This is the app</div>;
+  }
+}
+
+export default App;
